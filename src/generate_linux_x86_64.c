61ed6ad32163dbc8480a94e72668e54a28f2105c
Michael Kohn <mike@mikekohn.net>
2016-03-03 21:24:38 -0600

    Checking strlen() works.

--- src/generate_linux_x86_64.c
+++ src/generate_linux_x86_64.c
@@ -9,7 +9,11 @@
  *
  */
 
+#include <string.h>
+
 #include "generate.h"
+
+static int generate_strlen(struct _generate *generate, int len);
 
 int generate_init(struct _generate *generate, uint8_t *code)
 {
@@ -52,9 +56,9 @@ int generate_init(struct _generate *generate, uint8_t *code)
   generate->code[generate->ptr++] = 0xfe;
 
   // mov rax, rsi:  0x48  0x89  0xF0
-  generate->code[generate->ptr++] = 0x48;
-  generate->code[generate->ptr++] = 0x89;
-  generate->code[generate->ptr++] = 0xf0;
+  //generate->code[generate->ptr++] = 0x48;
+  //generate->code[generate->ptr++] = 0x89;
+  //generate->code[generate->ptr++] = 0xf0;
 
   return 0;
 }
@@ -71,6 +75,10 @@ int generate_not(struct _generate *generate)
 
 int generate_startswith(struct _generate *generate, char *match)
 {
+  int len = strlen(match);
+
+  generate_strlen(generate, len);
+
   return 0;
 }
 
@@ -91,14 +99,12 @@ int generate_contains(struct _generate *generate, char *match)
 
 int generate_finish(struct _generate *generate)
 {
-#if 0
   // mov eax, 1: 0xB8  0x01  0x00  0x00  0x00
   generate->code[generate->ptr++] = 0xb8;
   generate->code[generate->ptr++] = 0x01;
   generate->code[generate->ptr++] = 0x00;
   generate->code[generate->ptr++] = 0x00;
   generate->code[generate->ptr++] = 0x00;
-#endif
 
   // ret: 0xc3
   generate->code[generate->ptr++] = 0xc3;
@@ -106,4 +112,45 @@ int generate_finish(struct _generate *generate)
   return 0;
 }
 
+static int generate_strlen(struct _generate *generate, int len)
+{
+  if (len < 128)
+  {
+    // cmp rsi, 4:  0x48  0x83  0xFE  0x04
+    generate->code[generate->ptr++] = 0x48;
+    generate->code[generate->ptr++] = 0x83;
+    generate->code[generate->ptr++] = 0xfe;
+    generate->code[generate->ptr++] = len;
+  }
+    else
+  {
+    // cmp rsi, 200:  0x48  0x81  0xFE  0xC8  0x00  0x00  0x00
+    generate->code[generate->ptr++] = 0x48;
+    generate->code[generate->ptr++] = 0x81;
+    generate->code[generate->ptr++] = 0xfe;
+    generate->code[generate->ptr++] = len & 0xff;
+    generate->code[generate->ptr++] = (len >> 8) & 0xff;
+    generate->code[generate->ptr++] = (len >> 16) & 0xff;
+    generate->code[generate->ptr++] = (len >> 24) & 0xff;
+  }
+
+  // jge exit: 0x73 0x03
+  generate->code[generate->ptr++] = 0x7d;
+  generate->code[generate->ptr++] = 0x01;
+
+  // ret: 0xc3
+  generate->code[generate->ptr++] = 0xc3;
+
+  // jg exit_false: 0x7f 0xZZ (short)
+  // jg exit_false: 0x0f 0x8f 0xZZ 0xZZ (near)
+#if 0
+  generate->code[generate->ptr++] = 0x0f;
+  generate->code[generate->ptr++] = 0x8f;
+  generate->code[generate->ptr++] = 0x00;
+  generate->code[generate->ptr++] = 0x00;
+  generate->exit_list[generate->exit_list_ptr++] = generate->ptr;
+#endif
+
+  return 0;
+}
 
