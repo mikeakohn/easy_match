514a6fcf0ca592cdcfcfe3c6621cbb08610c0898
Michael Kohn <mike@mikekohn.net>
2020-11-06 05:20:07 -0600

    Fix Python scripts for python3.

--- scripts/get_opcodes_arm.py
+++ scripts/get_opcodes_arm.py
@@ -3,10 +3,10 @@
 import sys, os
 
 if len(sys.argv) != 2:
-  print "Usage: " + sys.argv[0] + " <asm>"
+  print("Usage: " + sys.argv[0] + " <asm>")
   sys.exit(0)
 
-out = open("/tmp/test.asm", "wb")
+out = open("/tmp/test.asm", "w")
 out.write(".arm\n")
 out.write("  " + sys.argv[1] + "\n\n")
 out.close()
@@ -15,10 +15,12 @@ os.system("naken_asm -b -o /tmp/test.bin /tmp/test.asm")
 
 code = []
 fp = open("/tmp/test.bin", "rb")
-for a in fp.readline():
+while True:
+  a = fp.read(1)
+  if not a: break
   code.append("0x%02x" % ord(a))
 fp.close()
 
-print "  // " + sys.argv[1] + ": " + ",".join(code)
-print "  generate_code(generate, " + str(len(code)) + ", " + ", ".join(code) + ");\n"
+print("  // " + sys.argv[1] + ": " + ",".join(code))
+print("  generate_code(generate, " + str(len(code)) + ", " + ", ".join(code) + ");\n")
 
